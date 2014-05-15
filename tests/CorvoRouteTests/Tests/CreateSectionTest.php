2e4041db261f2bd88949b02bb431f36821405b8c
Lisandro Seijo <lisandro_cuervo@hotmail.com>
2014-05-15 00:28:16 -0300

    Add test to create:section command

---
+++ tests/CorvoRouteTests/Tests/CreateSectionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Corvo\Routes\Components\CorvoRoutes;
+use Corvo\Routes\Commands\CreateSectionCommand;
+
+class CreateSectionTest extends TestCase {
+
+    public function testCreateSection()
+    {
+        $corvoRoutes = new CorvoRoutes;
+
+        $config  = $corvoRoutes->getConfig();
+        $basePath    = $config['base_path'];
+
+        $section = $basePath.'/MyNewSectionToTest';
+        
+        $pathsToCreate = array(
+            'section'     => $section,
+            'controllers' => $section.'/Controllers',
+            'models'      => $section.'/Models',
+            'views'       => $section.'/Views'
+        );
+
+        $routeFile = $section.'/'.$config['routes_filename'];
+        
+        Artisan::call('corvo:section', array('section_name' => 'MyNewSectionToTest'));
+
+        foreach($pathsToCreate as $path)
+        {
+            $this->assertTrue(
+                is_dir($path)
+            );
+        }
+
+        $this->assertTrue(is_file($routeFile));
+
+        File::deleteDirectory($section);
+    }
+}
